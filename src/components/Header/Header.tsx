d61f9d8ecfee6f94b621cad29bb8108f742bce37
Логунова Алина Дмитриевна <alina.logunova@norbit.ru>
2025-07-13 23:05:12 +0300

    feat: add styles for Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,22 @@
 import { Component } from 'react';
 import type { Props } from './types';
 
-export class Header extends Component<Props, object> {
+export class Header extends Component<Props> {
   render() {
     const { value, onChangeValue, onSearch } = this.props;
 
     return (
       <div className="flex flex-col items-center gap-2">
         <h1 className="text-2xl">Hello, It is Pokemon API</h1>
-        <div>
+        <div className="border border-blue-200">
           <input
-            className="p-2"
+            className="p-2 focus:outline-blue-300"
             type="text"
             placeholder="I'm searching..."
             value={value}
             onChange={onChangeValue}
           />
-          <button onClick={onSearch} className="p-2">
+          <button onClick={onSearch} className="p-2 bg-blue-200">
             Search
           </button>
         </div>
