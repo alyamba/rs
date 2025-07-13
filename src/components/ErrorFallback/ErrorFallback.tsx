5c91402a44364c0665091ddad0c50460f5cefb09
Логунова Алина Дмитриевна <alina.logunova@norbit.ru>
2025-07-13 17:14:36 +0300

    feat: add styles for ErrorFallback

--- src/components/ErrorFallback/ErrorFallback.tsx
+++ src/components/ErrorFallback/ErrorFallback.tsx
@@ -4,11 +4,17 @@ import errorCharacter from '../../assets/errorcharacter.png';
 export class ErrorFallback extends Component {
   render() {
     return (
-      <div>
-        <h2>Oops! Something went wrong.</h2>
-        <p>Please try again later.</p>
-        <div>
-          <img src={errorCharacter} alt="errorCharacter" />
+      <div className="flex flex-col items-center justify-center p-10 h-screen gap-4">
+        <p className="text-5xl font-semibold text-blue-950">
+          Oops! Something went wrong.
+        </p>
+        <p className="text-2xl text-blue-950">Please, try again later.</p>
+        <div className="h-100 ">
+          <img
+            src={errorCharacter}
+            alt="errorCharacter"
+            className="h-100 w-auto rounded-md"
+          />
         </div>
       </div>
     );
