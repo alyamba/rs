da50f5b72f51965095aba00822fc86d4e026889d
Логунова Алина Дмитриевна <alina.logunova@norbit.ru>
2025-07-20 03:55:38 +0300

    fix: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+    setupFiles: './setupTests.ts',
     globals: true,
     coverage: {
       thresholds: {
