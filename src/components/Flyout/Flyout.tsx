a27bd5f11a48bf8a452b3ba10bafcfaffabf8804
Логунова Алина Дмитриевна <alina.logunova@norbit.ru>
2025-08-02 14:07:15 +0300

    feat: add pokemon downloading

--- src/components/Flyout/Flyout.tsx
+++ src/components/Flyout/Flyout.tsx
@@ -11,6 +11,30 @@ export const Flyout: FC<FlyoutProps> = ({
   };
   const handleClickDownload = () => {};
 
+  const downloadInfo = [
+    [
+      'id',
+      'Pokemon',
+      'Height (m)',
+      'Weight (kg)',
+      'Image URL',
+      'Default pokemon',
+      'Types',
+    ],
+    ...selectedPokemons.map((pokemon) => [
+      pokemon.id,
+      pokemon.name,
+      pokemon.data.height / 10,
+      pokemon.data.weight / 10,
+      pokemon.data.imgUrl,
+      pokemon.data.isDefault ? 'Yes' : 'No',
+      pokemon.data.types,
+    ]),
+  ]
+    .map((row) => row.join(', '))
+    .join('\n');
+  const file = new Blob([downloadInfo], { type: 'text/csv' });
+
   return (
     <div
       className={`${styles.flyout} fixed z-2 w-full bottom-0 flex justify-between py-4 px-20 items-center gap-10 shadow-[0_-10px_15px_-3px_rgba(0,0,0,0.05)]`}
@@ -30,12 +54,14 @@ export const Flyout: FC<FlyoutProps> = ({
           Unselect all
         </button>
 
-        <button
+        <a
           className="p-2 border rounded cursor-pointer border-green-500 text-green-500 hover:bg-green-500 hover:text-white"
           onClick={handleClickDownload}
+          download={'pokemons.csv'}
+          href={URL.createObjectURL(file)}
         >
           Download
-        </button>
+        </a>
       </div>
     </div>
   );
