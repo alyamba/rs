6f3fe1cf28f4cc0f2afd8412ecd1e6cfee09a0ac
Логунова Алина Дмитриевна <alina.logunova@norbit.ru>
2025-08-08 00:00:39 +0300

    feat: replace fetch with RTK Query for CardDetails

--- src/components/CardDetails/CardDetails.tsx
+++ src/components/CardDetails/CardDetails.tsx
@@ -1,42 +1,16 @@
-import { useEffect, useState, type FC } from 'react';
+import { useState, type FC } from 'react';
 import { useNavigate, useParams, useSearchParams } from 'react-router';
-import { getPokeData } from '../../api/pokeAPI';
-import type { PokeData } from '../../api/types';
 import { Loading } from '../Loading';
 import styles from './CardDetailes.module.css';
+import { useGetPokemonByIdQuery } from '../../store';
 
 export const CardDetails: FC = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const params = useParams();
 
-  const [responseData, setResponseData] = useState<PokeData | null>(null);
   const [imageLoaded, setImageLoaded] = useState<boolean>(false);
-
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | undefined>(undefined);
-
-  const fetchPokeData = async (pokeId: string) => {
-    setLoading(true);
-    try {
-      const response = await getPokeData(pokeId);
-      setResponseData(response.data[0]);
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error(error.message);
-        setError(error.message);
-      } else {
-        console.error(String(error));
-        setError(`${error}`);
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchPokeData(String(params.id));
-  }, [params.id]);
+  const { data, isLoading, error } = useGetPokemonByIdQuery(String(params.id));
 
   const handleImageLoad = () => {
     setImageLoaded(true);
@@ -59,10 +33,10 @@ export const CardDetails: FC = () => {
         ×
       </button>
 
-      {loading ? (
+      {isLoading ? (
         <Loading />
-      ) : error ? (
-        <div className="text-center">{error}</div>
+      ) : error || !data ? (
+        <div className="text-center">Error</div>
       ) : (
         <div className="p-4">
           <div className="relative h-[280px]">
@@ -70,8 +44,8 @@ export const CardDetails: FC = () => {
 
             <div className="h-full flex items-center justify-center">
               <img
-                src={responseData?.data.imgUrl}
-                alt={responseData?.name}
+                src={data.data.imgUrl}
+                alt={data.name}
                 className={`h-full w-auto transition-opacity duration-300 ${
                   imageLoaded ? 'opacity-100' : 'opacity-0'
                 }`}
@@ -84,35 +58,35 @@ export const CardDetails: FC = () => {
           <div className="p-4 flex flex-col gap-2">
             <div className="flex flex-col justify-center items-center">
               <p className="text-2xl font-semibold" data-testid="card-name">
-                {responseData?.name.toUpperCase()}
+                {data.name.toUpperCase()}
               </p>
             </div>
 
             <div className="flex gap-1 items-center">
               <div className="text-l font-normal">Height: </div>
               <div className="text-l font-bold" data-testid="card-height">
-                {(responseData?.data.height || 0) / 10}M
+                {(data.data.height || 0) / 10}M
               </div>
             </div>
 
             <div className="flex gap-1 items-center">
               <div className="text-l font-normal">Weight: </div>
               <div className="text-l font-bold" data-testid="card-weight">
-                {(responseData?.data.height || 0) / 10}KG
+                {(data.data.height || 0) / 10}KG
               </div>
             </div>
 
             <div className="flex gap-1 items-center">
               <div className="text-l font-normal">Default pokemon: </div>
               <div className="text-l font-bold" data-testid="card-weight">
-                {responseData?.data.isDefault ? 'Yes' : 'No'}
+                {data.data.isDefault ? 'Yes' : 'No'}
               </div>
             </div>
 
             <div className="flex gap-1 items-center">
               <div className="text-l font-normal">Pokemon types: </div>
               <div className="text-l font-bold" data-testid="card-weight">
-                {responseData?.data.types.join(', ')}
+                {data.data.types.join(', ')}
               </div>
             </div>
           </div>
