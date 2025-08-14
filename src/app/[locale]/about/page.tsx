a528d48909b7d0a68a737eac86ba55e0c582e2a6
Логунова Алина Дмитриевна <alina.logunova@norbit.ru>
2025-08-14 23:46:09 +0300

    feat: update translation for AboutPage

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,6 +1,11 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 
 const AboutPage = () => {
+  const t = useTranslations('AboutPage');
+
   return (
     <div className="w-screen h-screen flex items-center gap-8">
       <div className="h-full flex flex-col gap-2 flex-3 justify-center text-xl p-12 relative hover:decoration-gray-600">
@@ -8,39 +13,44 @@ const AboutPage = () => {
           href="/"
           className="absolute z-1 top-10 text-base hover:underline hover:underline-offset-4 "
         >
-          Home
+          {t('linkToHome')}
         </Link>
         <div>
-          My name is <strong>Alina Logunova</strong> and I&apos;m frontend
-          developer.
+          {t.rich('authorNameText', {
+            name: () => <strong>{t('authorName')}</strong>,
+          })}
         </div>
         <div>
-          This website is my education task for{' '}
-          <a
-            href="https://rs.school/courses/reactjs"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="underline underline-offset-4 decoration-gray-400 hover:decoration-gray-600 transition-colors"
-          >
-            RSschool React course
-          </a>
-          .
+          {t.rich('rsCourse', {
+            course: (chunks) => (
+              <a
+                href="https://rs.school/courses/reactjs"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline underline-offset-4 decoration-gray-400 hover:decoration-gray-600 transition-colors"
+              >
+                {chunks}
+              </a>
+            ),
+          })}
         </div>
         <div>
-          If you want to see my other project, you can visit{' '}
-          <a
-            href="https://github.com/alyamba"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="underline underline-offset-4 decoration-gray-400 hover:decoration-gray-600 transition-colors"
-          >
-            my GitHub
-          </a>
-          .
+          {t.rich('otherProjects', {
+            github: () => (
+              <a
+                href="https://github.com/alyamba"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline underline-offset-4 decoration-gray-400 hover:decoration-gray-600 transition-colors"
+              >
+                {t('githubText')}
+              </a>
+            ),
+          })}
         </div>
       </div>
       <div className="flex-2 h-full flex items-center justify-center bg-sky-100">
-        <div className="rotate-90 text-7xl">ABOUT US</div>
+        <div className="rotate-90 text-7xl">{t('title')}</div>
       </div>
     </div>
   );
