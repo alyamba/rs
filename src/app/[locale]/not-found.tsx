6049f186dd4e87cb0ce52e8f63f37306604c640c
Логунова Алина Дмитриевна <alina.logunova@norbit.ru>
2025-10-14 11:17:24 +0300

    feat: change layout of not-found page

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -3,7 +3,7 @@ import pokemonError from '../../assets/pokemonError.gif';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function NotFound() {
+const NotFound = () => {
   const t = useTranslations('NotFoundPage');
 
   return (
@@ -13,17 +13,22 @@ export default function NotFound() {
         alt="pokemonError"
         className="absolute w-full h-auto object-cover z-1 opacity-25 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
       />
-      <div className="w-auto absolute z-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center text-center gap-4">
-        <div className="text-7xl sm:text-9xl font-bold">404</div>
-        <div className="text-4xl sm:text-5xl">{t('errorText')}</div>
+
+      <div className="h-full flex flex-col gap-2 flex-3 justify-center text-xl p-12 relative hover:decoration-gray-600">
         <Link
           href="/"
-          className="border-blue-950 cursor-pointer py-2 px-4 rounded-lg text-md bg-sky-950 text-white hover:shadow-md"
-          data-testid="go-home-button"
+          className="absolute z-1 top-10 text-base hover:underline hover:underline-offset-4 "
         >
           {t('linkToHome')}
         </Link>
+
+        <div className="w-auto absolute z-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center text-center gap-4">
+          <div className="text-7xl sm:text-9xl font-bold">404</div>
+          <div className="text-4xl sm:text-5xl">{t('errorText')}</div>
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default NotFound;
